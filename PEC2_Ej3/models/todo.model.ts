15a127d6e9e4fed517920cbf8ecf2dae6580ec4b
Omar Romera <oromeraa@uoc.edu>
2025-10-30 06:48:07 +0100

    Class todo.model to TS

---
+++ PEC2_Ej3/models/todo.model.ts
@@ -0,0 +1,28 @@
+/**
+ * @class Model 
+ * 
+ * Manages the data of the application.
+ */
+
+
+class Todo {
+    id: string;
+    text: string;
+    complete: boolean;
+
+    constructor(text: string = '', complete: boolean = false) {
+        this.id = this.uuidv4();
+        this.text = text;
+        this.complete = complete;
+    }
+
+    private uuidv4(): string {
+        const base: string = ([1e7] + -1e3 + -4e3 + -8e3 + -1e11);
+        let replaced: string = base.toString().replace(/[018]/g, (c: string) => {
+            let rand: number = crypto.getRandomValues(new Uint8Array(1))[0];
+            rand = rand & (15 >> (parseInt(c) / 4));
+            return (parseInt(c) ^ rand).toString(16);
+        });
+        return replaced;
+    }
+}
