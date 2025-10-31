1ce54545903ff08742221aa1da72906c5300609a
Omar Romera <oromeraa@uoc.edu>
2025-10-31 14:12:28 +0100

    App script file to TS

--- PEC2_Ej3/app.js
+++ PEC2_Ej3/app.js
@@ -1,1 +1,6 @@
-const app = new TodoController(new TodoService(), new TodoView());
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todo_controller_1 = require("./controllers/todo.controller");
+const todo_service_1 = require("./services/todo.service");
+const todo_views_1 = require("./views/todo.views");
+const app = new todo_controller_1.TodoController(new todo_service_1.TodoService(), new todo_views_1.TodoView());
